a0cb213156a2a9a32175315ee89c30b1bee7febb
bjornivar <bihoie@stud.ntnu.no>
2026-02-12 16:26:39 +0100

    Update InfoPath constant to include code parameter in URL

--- utils/constants.go
+++ utils/constants.go
@@ -4,7 +4,7 @@ package utils
 
 const DefaultPath = "/"
 const ExchangePath = "/exchange"
-const InfoPath = "/info"
+const InfoPath = "/info/{code}"
 const StatusPath = "/status"
 const CurrenciesApiUrl = "http://129.241.150.113:9090/currency/NOK"
 const RestCountriesApiUrl = "http://129.241.150.113:8080/v3.1/alpha/no" // Only checks Norway to see if the API is up
