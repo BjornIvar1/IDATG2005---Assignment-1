42eeea74355fadf6942cc2935e45909c6841e89f
bjornivar <bihoie@stud.ntnu.no>
2026-02-12 16:39:19 +0100

    Added error handling to InfoHandler

--- handler/InfoHandler.go
+++ handler/InfoHandler.go
@@ -24,6 +24,7 @@ type InfoResponse struct {
 // InfoHandler
 // Takes the country code as a path parameter, and makes a GET request to the API,
 // and returns the information from the country
+// If the country code is not valid, it returns an error message with status code 404
 func InfoHandler(w http.ResponseWriter, r *http.Request) {
 	code := r.PathValue("code")
 	apiURL := "http://129.241.150.113:8080/v3.1/alpha/" + code
@@ -33,6 +34,12 @@ func InfoHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to fetch country information", http.StatusInternalServerError)
 		return
 	}
+
+	if resp.StatusCode != 200 {
+		http.Error(w, "Country code is not valid", http.StatusNotFound)
+		return
+	}
+
 	defer resp.Body.Close()
 
 	var countryInfo []InfoResponse
