890f5e70cc4b8b7c4d84f615347a92a6500633b1
bjornivar <bihoie@stud.ntnu.no>
2026-02-17 23:10:59 +0100

    Rename constants.go to Constants.go and add base URLs for countries and currencies

---
+++ utils/Constants.go
@@ -0,0 +1,12 @@
+package utils
+
+//URL Paths as constants
+
+const DefaultPath = "/"
+const ExchangePath = "/exchange/{code}"
+const InfoPath = "/info/{code}"
+const StatusPath = "/status"
+const CurrenciesApiUrl = "http://129.241.150.113:9090/currency/NOK"
+const RestCountriesApiUrl = "http://129.241.150.113:8080/v3.1/alpha/no"
+const RestCountriesApiUrlBase = "http://129.241.150.113:8080/v3.1/alpha/"
+const RestCurrenciesApiUrlBase = "http://129.241.150.113:9090/currency/"
