a826a4144feb5c091920cdcc6255a956a5e43b4e
bjornivar <bihoie@stud.ntnu.no>
2026-02-17 23:10:49 +0100

    Implement ExchangeHandler to fetch currency rates and country information

--- handler/ExchangeHandler.go
+++ handler/ExchangeHandler.go
@@ -1,49 +1,125 @@
 package handler
 
 import (
+	"IDATG2005---Assignment-1/structs"
+	"IDATG2005---Assignment-1/utils"
 	"encoding/json"
 	"net/http"
 )
 
-type ExchangeResponse struct {
-	BaseCurrency string             `json:"base_currency"`
-	ExchangeRate map[string]float64 `json:"exchange-rate"`
-}
-
-type ExchangeOutput struct {
-	Basecode string `json:"base_code"`
-	Rates    struct {
-		EUR float64 `json:"EUR"`
-		SEK float64 `json:"SEK"`
-		RUB float64 `json:"RUB"`
+// MapToExchangeOutput
+// Maps the response from the APIs to the ExchangeOutput struct
+func MapToExchangeOutput(c structs.ExchangeCountry, e structs.ExchangeResponse, b []string) structs.ExchangeOutput {
+	out := structs.ExchangeOutput{
+		Country:  c.Name.Common,
+		Basecode: e.BaseCode,
+		Rates:    nil,
 	}
+
+	if len(b) > 0 {
+		out.Rates = make(map[string]float64)
+		for _, borderCurrCode := range b {
+			if rate, exists := e.ExchangeRate[borderCurrCode]; exists {
+				out.Rates[borderCurrCode] = rate
+			}
+		}
+	}
+	return out
 }
 
 func ExchangeHandler(w http.ResponseWriter, r *http.Request) {
-	code := r.PathValue("code")
-	apiURL := "http://129.241.150.113:9090/currency/" + code
+	conCode := r.PathValue("code")
 
-	resp, err := http.Get(apiURL)
+	//Check Country API
+	apiURLCon := utils.RestCountriesApiUrlBase + conCode
+
+	conResp, err := http.Get(apiURLCon)
 	if err != nil {
 		http.Error(w, "Failed to fetch country information", http.StatusInternalServerError)
 		return
 	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
+	if conResp.StatusCode != http.StatusOK {
 		http.Error(w, "Country code is not valid", http.StatusNotFound)
 		return
 	}
 
-	var exchangeOutput ExchangeOutput
-	errJsonDecoder := json.NewDecoder(resp.Body).Decode(&exchangeOutput)
+	defer conResp.Body.Close()
 
-	if errJsonDecoder != nil {
+	var exchangeCountry []structs.ExchangeCountry
+	conErrJsonDecoder := json.NewDecoder(conResp.Body).Decode(&exchangeCountry)
+
+	if conErrJsonDecoder != nil {
 		http.Error(w, "Failed to retrieve country information", http.StatusInternalServerError)
 		return
 	}
 
-	output := exchangeOutput
+	// Get the currency code of the country
+	var currCode string
+	for key := range exchangeCountry[0].Currencies {
+		currCode = key
+		break
+	}
+
+	// Check Currency API
+	apiURLCur := utils.RestCurrenciesApiUrlBase + currCode
+	respCur, err := http.Get(apiURLCur)
+	if err != nil {
+		http.Error(w, "Failed to fetch currency information", http.StatusInternalServerError)
+		return
+	}
+	defer respCur.Body.Close()
+
+	if respCur.StatusCode != http.StatusOK {
+		http.Error(w, "Exchange rate code is not valid", http.StatusNotFound)
+		return
+	}
+
+	var exchangeResponse structs.ExchangeResponse
+	curErrJsonDecoder := json.NewDecoder(respCur.Body).Decode(&exchangeResponse)
+
+	if curErrJsonDecoder != nil {
+		http.Error(w, "Failed to retrieve currency information", http.StatusInternalServerError)
+		return
+	}
+
+	// Get Borders and currency code of the country
+	var borders []string
+	for _, borderCountry := range exchangeCountry[0].Borders {
+		apiURLBor := utils.RestCountriesApiUrlBase + borderCountry
+		respBor, err := http.Get(apiURLBor)
+
+		if err != nil {
+			http.Error(w, "Failed to fetch border country information", http.StatusInternalServerError)
+			return
+		}
+
+		defer respBor.Body.Close()
+
+		if respBor.StatusCode != http.StatusOK {
+			http.Error(w, "Country code is not valid", http.StatusNotFound)
+			return
+		}
+
+		var borderCountry []structs.ExchangeCountry
+		borErrJsonDecoder := json.NewDecoder(respBor.Body).Decode(&borderCountry)
+
+		if borErrJsonDecoder != nil {
+			http.Error(w, "Failed to retrieve currency information", http.StatusInternalServerError)
+			return
+		}
+
+		var borderCurrCode string
+		for key := range borderCountry[0].Currencies {
+			borderCurrCode = key
+			break
+		}
+
+		borders = append(borders, borderCurrCode)
+	}
+
+	//My API
+	output := MapToExchangeOutput(exchangeCountry[0], exchangeResponse, borders)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(output)
--- structs/ExhangeStruct.go
+++ structs/ExhangeStruct.go
@@ -1,1 +1,25 @@
 package structs
+
+type ExchangeResponse struct {
+	Country      string             `json:"country"`
+	BaseCode     string             `json:"base_code"`
+	ExchangeRate map[string]float64 `json:"rates"`
+}
+
+type ExchangeCountry struct {
+	Name struct {
+		Common   string `json:"common"`
+		Official string `json:"official"`
+	} `json:"name"`
+	Currencies map[string]struct {
+		Name   string `json:"name"`
+		Symbol string `json:"symbol"`
+	} `json:"currencies"`
+	Borders []string `json:"borders"`
+}
+
+type ExchangeOutput struct {
+	Country  string             `json:"country"`
+	Basecode string             `json:"base_code"`
+	Rates    map[string]float64 `json:"exchange-rate"`
+}
